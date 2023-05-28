5a86d567ce0571052ef0ada7d47d1d478ff69aa2
Niranjan <newninja18@gmail.com>
2023-05-28 16:57:45 +0530

    [nvim] Remove old neovim and vimscript based config and start new lua config with bare minimum and only for languages actually used

--- .config/nvim/lua/keymap.lua
+++
@@ -1,2 +0,0 @@
-vim.api.nvim_set_keymap('n','<leader>qf',[[<cmd>lua require'navigation'.toggle_quickfix_list(1)<CR>]],{noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<leader>qlf',[[<cmd>lua require'navigation'.toggle_quickfix_list(0)<CR>]],{noremap = true, silent = true})
