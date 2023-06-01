3ec244b5a60ae823c71c3339cabf6db7a49e503e
Niranjan <newninja18@gmail.com>
2023-06-01 21:06:05 +0530

    [nvim] add nvim cmp completion and works fine

--- .config/nvim/lua/core/lazy.lua
+++ .config/nvim/lua/core/lazy.lua
@@ -74,9 +74,10 @@ require('lazy').setup({
   {
     'hrsh7th/nvim-cmp',
     dependencies = {
-      'L3MON4D3/LuaSnip',
+      { 'L3MON4D3/LuaSnip', build = "make install_jsregexp" },
       'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
     },
   },
 }, {})
