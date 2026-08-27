8255e343c521948ba6d4e9403732bf8321b32c5d
Niranjan <newninja18@gmail.com>
2026-08-27 19:53:50 +0530

    [nvim] fix telescope keymap issues

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 require("core/lazy")
 require("core/options")
 require("core/keymaps")
--- .config/nvim/lua/plugins/extras.lua
+++ .config/nvim/lua/plugins/extras.lua
@@ -3,11 +3,6 @@ return {
 		"folke/trouble.nvim",
 		cmd = "Trouble",
 		opts = {},
-		config = function(_, opts)
-			require("trouble").setup(opts)
-			vim.keymap.set("n", "<leader>xx", "<Cmd>Trouble diagnostics toggle<CR>", { desc = "Trouble diagnostics" })
-			vim.keymap.set("n", "<leader>xw", "<Cmd>Trouble diagnostics toggle filter.buf=0<CR>", { desc = "Trouble workspace diagnostics" })
-		end,
 	},
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
